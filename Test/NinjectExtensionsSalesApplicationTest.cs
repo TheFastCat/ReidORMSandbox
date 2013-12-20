edccb5b2c2ae90845afcf68800bf1e4d9935ce89
Reid Peryam <TheFastCat@hotmail.com>
2013-12-20 09:21:01 -0700

    Adding Ninject unit tests to verify NinjectModule bindings loading as well as end-to-end integration functionality (for execution in local environment, not from the cloud -- yet).

--- Test/NinjectExtensionsSalesApplicationTest.cs
+++ Test/NinjectExtensionsSalesApplicationTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Ninject;
+using Ninject.Modules;
 using System.Diagnostics;
 using SalesApplication.Data.ORM;
 using SalesApplication.Data.ORM.Contract;
@@ -12,6 +13,7 @@ using SalesApplication.Data.Adapter.Contract;
 using SalesApplication.Data.Adapter.Legacy.SQLServer;
 using System.Data;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Test
 {
@@ -22,13 +24,57 @@ namespace Test
     public class NinjectExtensionsSalesApplicationTest
     {
         IKernel _kernel;
-        ISalesAppData _iSalesAppData;
-        IDbConnection _idbConnection;
+
+        abstract class ModuleBase : NinjectModule
+        {
+            protected readonly string CONNECTION_STRING;
+            protected readonly string CONFIGURATION_CONNECTION_STRING = "Capwair.Test";
+
+            protected ModuleBase()
+            {
+                //read required parameters from application configuration
+                //these settings are required at runtime to retrieve Ninject type bindings for the application automatically
+                //without them auto-binding will fail...
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                #region    Read Configuration Settings from App.Config
+                CONNECTION_STRING = ConfigurationManager.ConnectionStrings[CONFIGURATION_CONNECTION_STRING].ConnectionString;
+                #endregion Read Configuration Settings from App.Config
+
+                #region    Error on Missing
+                if (string.IsNullOrWhiteSpace(CONNECTION_STRING))
+                {
+                    string format = string.Format("Could not parse argument '{0}' value from Configuration AppSettings.",
+                        CONFIGURATION_CONNECTION_STRING);
+                    throw new ConfigurationErrorsException(format);
+                }
+                #endregion    Error on Missing
+            }
+        }
+
+        class DapperModule : ModuleBase
+        {
+            public override void Load()
+            {
+                Bind<IDbConnection>().To<SqlConnection>().InSingletonScope().WithConstructorArgument("connectionString", CONNECTION_STRING);
+                Bind<ISalesAppORM>().To<DapperAdapter>().InSingletonScope();
+                Bind<ISalesAppData>().To<CapwairData>().InSingletonScope();
+            }
+        }
+
+        class MassiveModule : ModuleBase
+        {
+            public override void Load()
+            {
+                Bind<IDbConnection>().To<SqlConnection>().InSingletonScope().WithConstructorArgument("connectionString", CONNECTION_STRING);
+                Bind<ISalesAppORM>().To<MassiveAdapter>().InSingletonScope();
+                Bind<ISalesAppData>().To<CapwairData>().InSingletonScope();
+            }
+        }
 
         [TestFixtureSetUp]
         public void TextFixtureSetup()
-        {
-             _kernel = new StandardKernel();         
+        {     
         }
 
         [TestFixtureTearDown]
@@ -40,7 +86,7 @@ namespace Test
         [SetUp]
         public void SetUp()
         {
-
+            _kernel = new StandardKernel();    
         }
 
         [TearDown]
@@ -48,23 +94,116 @@ namespace Test
         {
         }
 
-        [Description("Verifies Ninject.Extensions.SalesApplication.Api's bindings are as expected")]
+        [Description("Verifies a NinjectModule configured for CapwairData and Dapper ORM loads types as expected at runtime")]
         [Test]
-        public void NinjectCompositionTest()
+        public void NinjectCapwairDataAndDapperCompositionTest()
         {
-            _idbConnection = _kernel.TryGet<IDbConnection>();
-            Assert.IsNotNull(_idbConnection);
-            Assert.IsInstanceOf<SqlConnection>(_idbConnection);
-            Assert.IsNotNullOrEmpty(_idbConnection.ConnectionString);
-            Assert.AreEqual(ConnectionState.Closed, _idbConnection.State);
+            _kernel.Load(new DapperModule());
 
-            _iSalesAppData = _kernel.TryGet<ISalesAppData>();
-            Assert.IsNotNull(_iSalesAppData);
-            Assert.IsInstanceOf<CapwairData>(_iSalesAppData);
+            IDbConnection idbConnection = _kernel.TryGet<IDbConnection>();
+                Assert.IsNotNull(idbConnection);
+                Assert.IsInstanceOf<SqlConnection>(idbConnection);
+                Assert.IsNotNullOrEmpty(idbConnection.ConnectionString);
+                Assert.AreEqual(ConnectionState.Closed, idbConnection.State);
 
-            // verify that the impl of the ORM is what we expect
-            //Assert.AreEqual(typeof(DapperAdapter), _iSalesAppData.ORM);
-            Assert.AreEqual(typeof(MassiveAdapter), _iSalesAppData.ORM);
+            ISalesAppData iSalesAppData = _kernel.TryGet<ISalesAppData>();
+                Assert.IsNotNull(iSalesAppData);
+                Assert.IsInstanceOf<CapwairData>(iSalesAppData);
+
+            Assert.IsNotNull(iSalesAppData.ORM);
+                Assert.AreEqual(typeof(DapperAdapter), iSalesAppData.ORM);
+        }
+
+        [Description("Verifies a NinjectModule configured for CapwairData and Massive ORM loads types as expected at runtime")]
+        [Test]
+        public void NinjectCapwairDataAndMassiveCompositionTest()
+        {
+            _kernel.Load(new MassiveModule());
+
+            IDbConnection idbConnection = _kernel.TryGet<IDbConnection>();
+                Assert.IsNotNull(idbConnection);
+                Assert.IsInstanceOf<SqlConnection>(idbConnection);
+                Assert.IsNotNullOrEmpty(idbConnection.ConnectionString);
+                Assert.AreEqual(ConnectionState.Closed, idbConnection.State);
+
+            ISalesAppData iSalesAppData = _kernel.TryGet<ISalesAppData>();
+                Assert.IsNotNull(iSalesAppData);
+                Assert.IsInstanceOf<CapwairData>(iSalesAppData);
+
+                Assert.IsNotNull(iSalesAppData.ORM);
+                Assert.AreEqual(typeof(MassiveAdapter), iSalesAppData.ORM);
+        }
+
+        [Description("Verifies a NinjectModule configured for CapwairData and Dapper GetAllAddresses() from configured conn str as expected")]
+        [Category("LocalIntegration")]
+        [Test,Explicit]
+        public void NinjectCapwairDataAndDapperGetAllAddressesTest()
+        {
+            _kernel.Load(new DapperModule());
+            ISalesAppData iSalesAppData = _kernel.TryGet<ISalesAppData>();
+            IEnumerable<dynamic> addresses    = iSalesAppData.GetAllAddresses();
+                Assert.IsNotNull(addresses);
+                Assert.Greater(addresses.ToList().Count,0);
+        }
+
+        [Description("Verifies a NinjectModule configured for CapwairData and Dapper GetAllCustomers() from configured conn str as expected")]
+        [Category("LocalIntegration")]
+        [Test, Explicit]
+        public void NinjectCapwairDataAndDapperGetAllCustomersTest()
+        {
+            _kernel.Load(new DapperModule());
+            ISalesAppData iSalesAppData = _kernel.TryGet<ISalesAppData>();
+            IEnumerable<dynamic> addresses = iSalesAppData.GetAllAddresses();
+                Assert.IsNotNull(addresses);
+                Assert.Greater(addresses.ToList().Count, 0);
+        }
+
+        [Description("Verifies a NinjectModule configured for CapwairData and Dapper GetAllCustomers() from configured conn str as expected")]
+        [Category("LocalIntegration")]
+        [Test, Explicit]
+        public void NinjectCapwairDataAndDapperGetAllPhoneNumbersTest()
+        {
+            _kernel.Load(new DapperModule());
+            ISalesAppData iSalesAppData = _kernel.TryGet<ISalesAppData>();
+            IEnumerable<dynamic> phoneNumbers = iSalesAppData.GetAllPhoneNumbers();
+                Assert.IsNotNull(phoneNumbers);
+                Assert.Greater(phoneNumbers.ToList().Count, 0);
+        }
+
+        [Description("Verifies a NinjectModule configured for CapwairData and Massive GetAllAddresses() from configured conn str as expected")]
+        [Category("LocalIntegration")]
+        [Test, Explicit]
+        public void NinjectCapwairDataAndMassiveGetAllAddressesTest()
+        {
+            _kernel.Load(new MassiveModule());
+            ISalesAppData iSalesAppData = _kernel.TryGet<ISalesAppData>();
+            IEnumerable<dynamic> addresses = iSalesAppData.GetAllAddresses();
+                Assert.IsNotNull(addresses);
+                Assert.Greater(addresses.ToList().Count, 0);
+        }
+
+        [Description("Verifies a NinjectModule configured for CapwairData and Massive GetAllCustomers() from configured conn str as expected")]
+        [Category("LocalIntegration")]
+        [Test, Explicit]
+        public void NinjectCapwairDataAndMassiveGetAllCustomersTest()
+        {
+            _kernel.Load(new MassiveModule());
+            ISalesAppData iSalesAppData = _kernel.TryGet<ISalesAppData>();
+            IEnumerable<dynamic> addresses = iSalesAppData.GetAllAddresses();
+                Assert.IsNotNull(addresses);
+                Assert.Greater(addresses.ToList().Count, 0);
+        }
+
+        [Description("Verifies a NinjectModule configured for CapwairData and Massive GetAllCustomers() from configured conn str as expected")]
+        [Category("LocalIntegration")]
+        [Test, Explicit]
+        public void NinjectCapwairDataAndMassiveGetAllPhoneNumbersTest()
+        {
+            _kernel.Load(new MassiveModule());
+            ISalesAppData iSalesAppData = _kernel.TryGet<ISalesAppData>();
+            IEnumerable<dynamic> phoneNumbers = iSalesAppData.GetAllPhoneNumbers();
+                Assert.IsNotNull(phoneNumbers);
+                Assert.Greater(phoneNumbers.ToList().Count, 0);
         }
     }
 }
